8fbd6d543d616559fa4ce0670d2b73bebe7d4621
Jake Vanderplas <vanderplas@astro.washington.edu>
2013-03-18 11:15:19 -0700

    add animation stuff

---
+++ examples/basic_animation.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
+# First set up the figure, the axis, and the plot element we want to animate
+fig = plt.figure()
+ax = plt.axes(xlim=(0, 2), ylim=(-2, 2))
+line, = ax.plot([], [], lw=2)
+
+# initialization function: plot the background of each frame
+def init():
+    line.set_data([], [])
+    return line,
+
+# animation function.  This is called sequentially
+x = np.linspace(0, 2, 1000)
+
+def animate(i):
+    y = np.sin(2 * np.pi * (x - 0.01 * i))
+    line.set_data(x, y)
+    return line,
+
+# call the animator.  blit=True means only re-draw the parts that have changed.
+anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=10, interval=20, blit=True)
+
+plt.show()
